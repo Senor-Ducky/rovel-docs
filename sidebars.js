2b2853883b5e11cf353d8d65ce97f1e7e43fc0be
Rahul Mishra <robom460@gmail.com>
2020-12-24 18:15:50 +0530

    redone

--- sidebars.js
+++ sidebars.js
@@ -1,16 +1,5 @@
 module.exports = {
   someSidebar: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-    
-=======
-    RovelJS: ['Home'],
-    RovelAPI: ['mdx'],
->>>>>>> 94ff5ac7b50a94ef32b965125318efad7a34f133
-=======
-    RovelJS: ['rapi'],
-    RovelAPI: ['rjs'],
->>>>>>> 1ce52d578d8ea5430b13c0d497aca8367c019e82
+      RovelJs: ['Home']
   },
 };
